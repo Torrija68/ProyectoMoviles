6676e06f6fb5602ac5349035511ae5b81df90b4f
Leo <Leo@LOKINSPIRON05>
2023-12-20 23:04:15 +0100

    hola3

--- app/src/main/java/com/example/proyectomoviles/MainActivity.kt
+++ app/src/main/java/com/example/proyectomoviles/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.proyectomoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnIngresar: Button
+    private lateinit var btnRegistro: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnIngresar = findViewById(R.id.btnIngresar)
+        btnRegistro = findViewById(R.id.btnRegistrarse)
+
+        btnRegistro.setOnClickListener{
+            val intent = Intent(this,Login::class.java)
+            startActivity(intent)
+        }
+        btnIngresar.setOnClickListener {
+            val intent = Intent(this,Registro::class.java)
+            startActivity(intent)
+        }
     }
 
 
